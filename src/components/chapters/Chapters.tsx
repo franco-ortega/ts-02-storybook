8b9fc74c658eeefca68db4c7270abe8efb0435f8
Franco Ortega <francopdx@gmail.com>
2021-09-06 21:34:32 -0700

    add chapterOptions with test data

--- src/components/chapters/Chapters.tsx
+++ src/components/chapters/Chapters.tsx
@@ -7,12 +7,21 @@ interface Props {
 }
 
 const Chapters: React.FC<Props> = ({ userName }) => {
+
+  const testChoices: string[] = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+  // eslint-disable-next-line max-len
+  const chapterOptions: JSX.Element[] = testChoices.map((choice: string, i: number) => (
+    <li key={i}>{choice}</li>
+  ));
+
   return (
     <section className={styles.Chapters}>
       <h1>Chapters</h1>
       <p>
         Hello, {userName}. This is where you create your story.
       </p>
+      <ul>{chapterOptions}</ul>
     </section>
   );
 };
