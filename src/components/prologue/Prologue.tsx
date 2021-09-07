a58f070b5b2b0f6440add1ca8a8ea6e92c6ac83b
Franco Ortega <francopdx@gmail.com>
2021-09-06 19:32:21 -0700

    add event handler for Prologue form

--- src/components/prologue/Prologue.tsx
+++ src/components/prologue/Prologue.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router';
 
 interface Props {
   setUserName: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Prologue: React.FC<Props> = ({ setUserName }) => {
+  const history = useHistory();
+
+  // eslint-disable-next-line max-len
+  const onNameSubmit: React.FormEventHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    console.log('Name form clicked');
+    history.push('/chapters');
+  };
+
   return (
     <section>
       <h1>Prologue</h1>
       <p>Intro and Instructions</p>
-      <form action="">
-        <label htmlFor="">
-          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+      <form onSubmit={onNameSubmit}>
+        <label htmlFor="user-name">
+          <input
+            type="text"
+            id="user-name"
+            placeholder="Enter name"
+            onChange={(e) => setUserName(e.target.value)} />
         </label>
         <button>Submit</button>
       </form>
