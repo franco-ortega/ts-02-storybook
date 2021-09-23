eb93afe9b038173dbca75e62045deaed3404e393
Franco Ortega <francopdx@gmail.com>
2021-09-22 22:01:46 -0700

    refactor Story to use reduce instead of map for userStory

--- src/components/story/Story.tsx
+++ src/components/story/Story.tsx
@@ -7,15 +7,17 @@ interface Props {
 }
 
 const Story: React.FC<Props> = ({ userData }) => {
-  console.log(userData);
-  const userStory = userData.map((data, index) => (
-    <p key={index}>{data}</p>
-  ));
+  // Loop through userData selections and concatenate them
+  // together to create the story
+  const userStory = userData.reduce((previous, current, index) => {
+    if(index < userData.length - 1) return previous + current + ' ';
+    else return previous + current;
+  }, '');
 
   return (
     <main className={styles.Story}>
       <h1>Your story...</h1>
-      <div>{userStory}</div>
+      <p>{userStory}</p>
     </main>
   );
 };
